b7c5d4c8e1e6d552a0d518bcb8f9ce0ced31b6dd
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-20 10:56:18 +0300

    r

--- src/Ass/Tes.java
+++ src/Ass/Tes.java
@@ -5,14 +5,7 @@ public class Tes {
 	public static void main(String[] args) {
 		
 				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
-				System.out.println("Hello World");
+				
 				
 			
 
