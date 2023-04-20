bf6fc0154282a92b7d706306b2b249b31cb98ef5
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-20 11:08:25 +0300

    nn

---
+++ src/Ass/df.java
@@ -0,0 +1,16 @@
+package Ass;
+
+public class df {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+		
+	
+
+
+}
+
+}
